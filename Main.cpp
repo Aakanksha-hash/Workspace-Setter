fae5e9528bb6568a9dd5400ebbbb0d205f5bb31f
Mithil Kiran Poojary <mithil467@gmail.com>
2019-08-04 00:57:03 +0530

    Added in files

--- Main.cpp
+++ Main.cpp
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 char *to_str[] = {"1", "2", "3", "4", "5"};
 
@@ -27,7 +28,7 @@ void create_folders(char *folder_name, short n)
 void make_files(char *name, short n)
 {
     FILE *source, *target;
-    char ch;
+    char ch, tem_folder[50];
     for (int i = 0; i < n; ++i)
     {
 
@@ -35,6 +36,7 @@ void make_files(char *name, short n)
         strcat(name, "/");
         strcat(name, to_str[i]);
         strcat(name, "/");
+        strcpy(tem_folder, name);
         strcat(name, "Main.cpp");
         printf("Copying template ... \n");
         target = fopen(name, "w");
@@ -44,6 +46,10 @@ void make_files(char *name, short n)
         }
         fclose(source);
         fclose(target);
+        strcat(tem_folder, "in");
+        printf("Creating in ... \n");
+        target = fopen(tem_folder, "w");
+        fclose(target);
         name = strtok(name, "/");
     }
 }
@@ -52,8 +58,8 @@ int main(int args, char **argv)
 {
     char *folder_name = argv[1];
     short sub_folders = atoi(argv[2]);
-
+    char *user_name = getlogin();
+    printf("Hi %s!\n\n", user_name);
     create_folders(folder_name, sub_folders);
-
     make_files(folder_name, sub_folders);
 }
