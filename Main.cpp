d64922610fb98511183c2c4f02c5f804bf00e45e
Mithil Kiran Poojary <mithil467@gmail.com>
2019-08-03 23:31:29 +0530

    Can create fodler

--- Main.cpp
+++ Main.cpp
@@ -1,4 +1,16 @@
-int main()
+#include <stdio.h>
+#include <sys/stat.h>
+
+void create_folder(char* folder_name)
 {
-    
+    printf("Initializing work space %s \n",folder_name);
+    mkdir(folder_name, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+}
+
+int main(int args, char ** argv)
+{
+    char* folder_name = argv[1];
+
+    create_folder(folder_name);
+
 }
