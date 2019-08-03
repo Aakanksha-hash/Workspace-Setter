23f784c1f6da805c2d3bfc31533a4fa795674727
Mithil Kiran Poojary <mithil467@gmail.com>
2019-08-04 00:05:12 +0530

    Creating sub foldes

--- Main.cpp
+++ Main.cpp
@@ -1,16 +1,33 @@
 #include <stdio.h>
 #include <sys/stat.h>
+#include <stdlib.h>
+#include <string.h>
 
-void create_folder(char* folder_name)
+char *to_str[] = {"1", "2", "3", "4", "5"};
+
+void create_folders(char *folder_name, short n)
 {
-    printf("Initializing work space %s \n",folder_name);
-    mkdir(folder_name, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+    char *name = folder_name;
+
+    printf("Initializing work space %s \n", name);
+    mkdir(name, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+    strcat(name, "/");
+
+    for (int i = 0; i < n; ++i)
+    {
+        strcat(name, "/");
+        printf("Creating %s \n", to_str[i]);
+        mkdir(strcat(name, to_str[i]), S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+
+        name = strtok(name, "/");
+    }
 }
 
-int main(int args, char ** argv)
+int main(int args, char **argv)
 {
-    char* folder_name = argv[1];
+    char *folder_name = argv[1];
+    short sub_folders = atoi(argv[2]);
 
-    create_folder(folder_name);
-
+    create_folders(folder_name, sub_folders);
 }
