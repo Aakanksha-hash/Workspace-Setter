693a180abac3eb7bd871e0090ce2c7a15aa476c5
Aakanksha-hash <poojaryaakanksha@gmail.com>
2019-10-01 20:44:05 +0530

    Added default templates for python and java

---
+++ def.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Codechef
+{
+	public static void main(String args[])
+	{
+		int t;
+		Scanner sc = new Scanner(System.in);
+		t = sc.nextInt();
+		for(int i = 0;i<t;++i)
+		{
+			
+		}
+	}
+}
---
+++ def.py
@@ -0,0 +1,3 @@
+t = int(input())
+for _ in range(t):
+	arr = list(map(int, input().split()))
